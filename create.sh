c72e840857d4a0068615518d0a4f147466910526
deltasamantha <deltasamantha@gmail.com>
2026-02-15 18:56:16 +0200

    Upgraded branch

--- create.sh
+++ create.sh
@@ -21,7 +21,7 @@ set -euo pipefail
 
 VERSION="2.0.0"
 REPO_URL="https://github.com/deltasamantha/delta-starter.git"
-REPO_BRANCH="main"
+REPO_BRANCH="master"
 
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'
 BLUE='\033[0;34m'; CYAN='\033[0;36m'; MAGENTA='\033[0;35m'
